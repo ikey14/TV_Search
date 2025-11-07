19fe2d8fe6672f0aa65d620d2ae7e8ba2bfb989c
Enrique Iglesias <ike.umafi@gmail.com>
2025-11-07 16:30:58 +0100

    Somewhat started implementing the window with the react-modals library. I don't understand how it works right now but I'll fix it.

--- TV_World/src/components/Show.jsx
+++ TV_World/src/components/Show.jsx
@@ -1,15 +1,50 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Modal from 'react-modal';
+
 import "./Show.css";
 
 export default function Show({id, name, imgSrc, changeFavStatus})
 {
+    const [showWindow, setShowWindow] = useState(false);
 
-    return(<div style={{ display: 'flex', alignItems: 'center' }}>
+    const handleShowClick = (e) => {
+        // Prevent clicks on the button from triggering the info popup
+        if (e.target.tagName.toLowerCase() !== "button") {
+        setShowWindow(true);
+        }
+    };
+
+    const closeModal = () => setShowWindow(false);
+
+    Modal.setAppElement('#root');
+
+    return(<div style = {{ display: 'flex', alignItems: 'center' }} onClick = {handleShowClick}>
 
         <img className = "showImg" src = {imgSrc} alt = {imgSrc}/>
         <p className = "showName">{name}</p>
         <button onClick = {() => changeFavStatus(id)}>❤️</button>
+
+        <Modal
+        isOpen = {showWindow}
+        onRequestClose = {closeModal}
+        ariaHideApp = {false}
+        shouldCloseOnOverlayClick = {true}
+        >
+            <h2>Hello</h2>
+            <button onClick={closeModal}>X</button>
+            <div>I am a modal</div>
+      </Modal>
+        
+        {/* {showWindow && (
+        <div className = "showInfoPopup">
+          <button onClick={closeInfo}>X</button>
+          {/* You can render richer info here (e.g. image, description, etc.)}
+          <h3>{name}</h3>
+          <img src = {imgSrc} alt = {imgSrc} style = {{ width: 80, height: 80 }} />
+          <p>Additional information goes here.</p>
+        </div>
+      )} */}
 
     </div>)
 }
